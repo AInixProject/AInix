68b9f1afb424bf7a32711531cb65798f424240fd
David Gros <dngros@live.com>
2018-06-21 23:20:04 -0700

    very primitive aish prompt that just runs based off simple splitting

--- aish/aish.py
+++ aish/aish.py
@@ -3,9 +3,31 @@ from prompt_toolkit import PromptSession
 from pygments.lexers import BashLexer
 from prompt_toolkit.shortcuts import prompt
 from prompt_toolkit.lexers import PygmentsLexer
+import pygments
+from pygments.token import Token
+import subprocess
 
-# Create prompt object.
+
 session = PromptSession()
+myLexer = BashLexer()
+
 while True:
     text = session.prompt('$ ', lexer=PygmentsLexer(BashLexer))
-    print(text)
+    lexed = pygments.lex(text, myLexer)
+    words = []
+    error = False
+    for tokenType, value in lexed:
+        if tokenType in (Token.Text, Token.Literal.Number):
+            stripped = value.strip()
+            if len(stripped) > 0:
+                words.append(value)
+        else:
+            print("AAAHHHH", tokenType, value)
+            error = True
+            break
+    if not error:
+        try:
+            cmdProc = subprocess.Popen(words)
+            cmdProc.wait()
+        except Exception as e:
+            print(e)
