fix some issues with average vectorizers. Start work on decodrs

ainix_kernel/models/EncoderDecoder/encoders.py
@@ -6,7 +6,6 @@ from typing import Iterable, Tuple, Type
 from ainix_kernel.model_util.tokenizers import Tokenizer
 from ainix_kernel.model_util.vectorizers import VectorizerBase
 from ainix_kernel.model_util.vocab import Vocab
-from ainix_kernel.model_util.rnn_encoder import EncoderRNN
 
 
 class QueryEncoder(nn.Module, ABC):
@@ -99,6 +98,7 @@ class RNNSeqEncoder(VectorSeqEncoder):
         self,
         input_dims: int,
         hidden_size: int,
+        summary_size: int,
         rnn_cell: Type = nn.GRU,
         num_layers: int = 1,
         input_dropout_p: float = 0,
@@ -112,6 +112,8 @@ class RNNSeqEncoder(VectorSeqEncoder):
         self.rnn = self.rnn_cell(input_dims, hidden_size, num_layers, num_layers,
                                  batch_first=True, dropout=dropout_p,
                                  bidirectional=bidirectional)
+        # Actual hidden size will be twice size since bidirectional
+        self.summary_linear = nn.Linear(hidden_size*2, summary_size)
 
     def forward(self, seqs: torch.Tensor, input_lengths=None) -> Tuple[torch.Tensor, torch.Tensor]:
         # NOTE (DNGros): this was just copied from the IBM implementation.
@@ -120,24 +122,25 @@ class RNNSeqEncoder(VectorSeqEncoder):
         seqs = self.input_dropout(seqs)
         if self.variable_lengths:
             seqs = nn.utils.rnn.pack_padded_sequence(seqs, input_lengths, batch_first=True)
-        output, hidden = self.rnn(seqs)
+        outputs, final_hiddens = self.rnn(seqs)
         if self.variable_lengths:
-            output, _ = nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
+            outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs, batch_first=True)
             raise NotImplemented("Make sure this is working as expected")
-        # As the "summary value" we return the last value in the sequence
-        return output[:, -1, :], output
+        summaries = self.summary_linear(final_hiddens)
+        return summaries, outputs
 
 
 def make_default_query_encoder(
     x_tokenizer: Tokenizer,
     query_vocab: Vocab,
     query_vectorizer: VectorizerBase,
-    output_size = 64
+    output_size=64
 ) -> QueryEncoder:
     """Factory for making a default QueryEncoder"""
     internal_encoder = RNNSeqEncoder(
         query_vectorizer.feature_len(),
         output_size,
+        output_size
     )
     return StringQueryEncoder(x_tokenizer, query_vocab, query_vectorizer, internal_encoder)
 

ainix_kernel/tests/test_model_util/test_vectorizers.py
@@ -2,29 +2,29 @@ from unittest.mock import MagicMock
 from ainix_kernel.model_util.vectorizers import *
 
 def test_avging():
-    mockvocab = MagicMock()
     def x_vec_func(x):
         if x == "4":
             return torch.Tensor([4, 4, 4, 8, 8])
         else:
             return torch.Tensor([2, 2, 2, 2, 2])
-    mockvocab.token_to_index = lambda x: 1
+    mockvocab = MagicMock(spec=Vocab)
+    mockvocab.token_seq_to_indices = MagicMock(return_value=[1, 1])
     mockvocab.__len__ = lambda self: 3
+    mocktokenizer = MagicMock()
 
-    proc_func = make_xquery_avg_pretrain_func(mockvocab, x_vec_func)
+    proc_func = make_xquery_avg_pretrain_func(mockvocab, mocktokenizer, x_vec_func)
     vectorizer = PretrainedAvgVectorizer(5, proc_func, mockvocab)
 
     pretrainer = vectorizer.get_pretrainer()
     mockast = MagicMock()
     mockexample = MagicMock()
-    mockast.depth_first_iter = lambda: ["a", "b"]
     pretrainer.pretrain_example(mockexample, mockast)
     mockexample.xquery = "4"
-    mockvocab.token_to_index = lambda x: 0
+    mockvocab.token_seq_to_indices = MagicMock(return_value=[0, 0])
     pretrainer.pretrain_example(mockexample, mockast)
-    mockvocab.token_to_index = lambda x: 2
+    mockvocab.token_seq_to_indices = MagicMock(return_value=[2, 2])
     pretrainer.pretrain_example(mockexample, mockast)
-    mockvocab.token_to_index = lambda x: 1
+    mockvocab.token_seq_to_indices = MagicMock(return_value=[1, 1])
     pretrainer.pretrain_example(mockexample, mockast)
     pretrainer.close()
     assert torch.all(vectorizer(torch.LongTensor([[0, 1, 2]])).eq(
@@ -32,4 +32,4 @@ def test_avging():
                           [3, 3, 3, 5, 5],
                           [4, 4, 4, 8, 8]]])
     ))
-    assert torch.all(vectorizer.counts.eq(torch.Tensor([2, 4, 2])))
+    assert torch.all(vectorizer.counts.eq(torch.Tensor([2.0, 4.0, 2.0])))