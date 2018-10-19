4d0e308da92a7f12b9bc8d0d243b7fdc1c4d65b9
David Gros <dngros@live.com>
2018-10-18 23:40:39 -0500

    fix some broken tests

--- builtin_types/test_posix_parsers.py
+++ builtin_types/test_posix_parsers.py
@@ -13,20 +13,29 @@ def type_context():
     return context
 
 
+def gen_result(gen):
+    """Get the result from a generator when expected to return next."""
+    try:
+        next(gen)
+    except StopIteration as stp:
+        return stp.value
+    raise ValueError("Expected it to stop")
+
+
 def test_cmd_seq_parser(type_context):
     cmd_seq_obj = type_context.get_object_by_name("CommandSequenceObj")
     instance = type_context.get_object_parser_by_name("CmdSeqParser")
-    result = instance.parse_string("hello", cmd_seq_obj)
+    result = gen_result(instance.parse_string("hello", cmd_seq_obj))
     assert result.get_arg_present("ProgramArg").slice == (0, 5)
     assert result.get_arg_present("CompoundOp") is None
 
-    result = instance.parse_string("woo | foo", cmd_seq_obj)
+    result = gen_result(instance.parse_string("woo | foo", cmd_seq_obj))
     assert result.get_arg_present("ProgramArg").slice == (0, 4)
     assert result.get_arg_present("ProgramArg").slice_string == "woo"
     assert result.get_arg_present("CompoundOp").slice == (4, 9)
     assert result.get_arg_present("CompoundOp").slice_string == "| foo"
 
-    result = instance.parse_string("woo | foo | go", cmd_seq_obj)
+    result = gen_result(instance.parse_string("woo | foo | go", cmd_seq_obj))
     assert result.get_arg_present("ProgramArg").slice == (0, 4)
     assert result.get_arg_present("ProgramArg").slice_string == "woo"
     assert result.get_arg_present("CompoundOp").slice == (4, 14)
@@ -36,13 +45,13 @@ def test_cmd_seq_parser(type_context):
 def test_cmd_seq_parser_quotes(type_context):
     cmd_seq_obj = type_context.get_object_by_name("CommandSequenceObj")
     instance = type_context.get_object_parser_by_name("CmdSeqParser")
-    result = instance.parse_string('hello "pi |" | foo', cmd_seq_obj)
+    result = gen_result(instance.parse_string('hello "pi |" | foo', cmd_seq_obj))
     assert result.get_arg_present("ProgramArg").slice == (0, 13)
     assert result.get_arg_present("ProgramArg").slice_string == 'hello "pi |"'
     assert result.get_arg_present("CompoundOp").slice == (13, 18)
     assert result.get_arg_present("CompoundOp").slice_string == "| foo"
 
-    result = instance.parse_string(r'he "\"pi\" |" | foo', cmd_seq_obj)
+    result = gen_result(instance.parse_string(r'he "\"pi\" |" | foo', cmd_seq_obj))
     assert result.get_arg_present("ProgramArg").slice == (0, 14)
     assert result.get_arg_present("ProgramArg").slice_string == r'he "\"pi\" |"'
     assert result.get_arg_present("CompoundOp").slice == (14, 19)
@@ -104,7 +113,7 @@ def test_prog_object_parser_basic(type_context):
         type_context, "FooProgram", "Program",
         [AInixArgument(type_context, "a", None, arg_data = {"short_name": "a"})])
     parser = type_context.get_object_parser_by_name("ProgramObjectParser")
-    result = parser.parse_string("-a", onearg)
+    result = gen_result(parser.parse_string("-a", onearg))
     assert result.get_arg_present("a") is not None
     assert result.get_arg_present("a").slice_string == ""
 
@@ -115,12 +124,12 @@ def test_prog_object_parser_argval(type_context):
         type_context, "FooProgram", "Program",
         [AInixArgument(type_context, "a", fooType.name, arg_data={"short_name": "a"})])
     parser = type_context.get_object_parser_by_name("ProgramObjectParser")
-    result = parser.parse_string("-a hello", argval)
+    result = gen_result(parser.parse_string("-a hello", argval))
     assert result.get_arg_present("a") is not None
     assert result.get_arg_present("a").slice_string == "hello"
 
     # Combined style
-    result = parser.parse_string("-afoo", argval)
+    result = gen_result(parser.parse_string("-afoo", argval))
     assert result.get_arg_present("a") is not None
     assert result.get_arg_present("a").slice_string == "foo"
 
@@ -132,9 +141,9 @@ def test_prog_object_parser_twoargs(type_context):
          AInixArgument(type_context, "barg", None, arg_data={"short_name": "b"})]
     )
     parser = type_context.get_object_parser_by_name("ProgramObjectParser")
-    result = parser.parse_string("-a -b", twoargs)
+    result = gen_result(parser.parse_string("-a -b", twoargs))
     assert result.get_arg_present("a") is not None
     assert result.get_arg_present("barg") is not None
-    result = parser.parse_string("-b", twoargs)
+    result = gen_result(parser.parse_string("-b", twoargs))
     assert result.get_arg_present("a") is None
     assert result.get_arg_present("barg") is not None
