679d0450c38e6ed6723dd80fd7f9cb91648badc7
David Gros <dngros@live.com>
2018-11-14 00:52:36 -0600

    add missing file

---
+++ ainix_common/tests/test_copy_tools.py
@@ -0,0 +1,21 @@
+from ainix_common.parsing.copy_tools import *
+
+
+def test_substring():
+    res = string_in_tok_list("hello there", ["hello", " there"])
+    assert res == (0, 1)
+
+
+def test_substring2():
+    res = string_in_tok_list("hello there", ["why", " ", "hello", " ", "there", " ", "friend"])
+    assert res == (2, 4)
+
+
+def test_substring3():
+    res = string_in_tok_list("hello", ["why", " ", "hello", " ", "there", " ", "friend"])
+    assert res == (2, 2)
+
+
+def test_substring4():
+    res = string_in_tok_list("ello there", ["why", " ", "hello", " ", "there", " ", "friend"])
+    assert res is None
