AInixProject/AInix

commit 40569e672b1f90e724e874e89533569fd3e22de3

fix some bugs with extra spaces in unparses

ainix_common/parsing/parse_primitives.py
@@ -276,11 +276,11 @@ class TypeToStringResult:
     def add_string(self, string: str):
         self._unparse_seq.append(string)
 
-    def add_impl_unparse(self):
+    def add_impl_unparse(self, prefix_adder: Callable[[str], str] = None):
         if self._already_added_impl:
             raise ValueError("Cannot add implementation multiple times in unparse")
         next_parser = _next_parser_of_impl(self._implementation, self._type_of_impl)
-        to_string_del = ImplementationToStringDelegation(next_parser)
+        to_string_del = ImplementationToStringDelegation(next_parser, prefix_adder)
         self._unparse_seq.append(to_string_del)
         self._already_added_impl = True
 
@@ -309,8 +309,16 @@ class ImplementationParseDelegation:
 
 @attr.s(auto_attribs=True, frozen=True)
 class ImplementationToStringDelegation:
-    """Used in an unparse to represent the slot where the implentation goes"""
+    """Used in an unparse to represent the slot where the implentation goes
+
+    Args:
+        next_parser: the next object parser to use
+        prefix_adder: A function is called to optionally return a prefix
+            to the result. It is called with the result of next_parser.
+            The string it returns is added before it.
+    """
     next_parser: 'ObjectParser'
+    prefix_adder: Optional[Callable[[str], str]] = None
 
 
 # Monstrosity that describes the type of a parser_func for Object parsers.

ainix_common/parsing/stringparser.py
@@ -355,6 +355,9 @@ class AstUnparser:
                 impl_string = self._unparse_object_node(
                     node.next_node_not_copy, part_of_out.next_parser, result_builder,
                     new_left_offset, child_nums_here + (0, ))
+                if part_of_out.prefix_adder:
+                    prefix = part_of_out.prefix_adder(impl_string)
+                    impl_string = prefix + impl_string
                 out_string += impl_string
                 new_left_offset += len(impl_string)
             else:

builtin_types/posix_parsers.py
@@ -109,8 +109,10 @@ def ProgramTypeParser(
 def ProgramTypeUnparser(result: parse_primitives.TypeToStringResult):
     impl = result.implementation
     prog_name = impl.type_data['invoke_name']
-    result.add_string(prog_name + " ")
-    result.add_impl_unparse()
+    result.add_string(prog_name)
+    result.add_impl_unparse(
+        prefix_adder=lambda s: " " if s else ""
+    )
 ####
 
 
@@ -321,6 +323,8 @@ def ProgramObjectUnparser(
         had_prev_args = True
     # Now do all the positional args in order
     for arg in get_all_positional_args(arg_map.implementation.children):
+        if not arg_map.is_argname_present(arg.name):
+            continue
         if had_prev_args:
             result.add_string(" ")
         result.add_arg_tostring(arg)

builtin_types/test_posix_parsers.py
@@ -1,3 +1,5 @@
+import random
+
 import pytest
 
 from ainix_common.parsing.grammar_lang import create_object_parser_from_grammar
@@ -22,7 +24,7 @@ def _make_positional(name="p1", position: int = 0,
     """Shorthand for constructing a positional argument"""
     return AInixArgument(MagicMock(), name, "FooType",
                          arg_data={POSITION: position, MULTIWORD_POS_ARG: multiword},
-                         required=required)
+                         required=required, parent_object_name=str(random.randint(1, 999)))
 
 
 @pytest.fixture(scope="function")
@@ -253,6 +255,27 @@ def test_prog_object_parser_posarg_multword(type_context):
     assert result.remaining_start_i == len("hello yo there")
 
 
+def test_prog_object_parser_posarg_multword_optional(type_context):
+    argval = AInixObject(
+        type_context, "FooProgram", "Program",
+        [_make_positional(required=False, multiword=True)])
+    parser = type_context.get_object_parser_by_name("ProgramObjectParser")
+    result = gen_result(parser.parse_string("hello yo there", argval))
+    assert result.get_arg_present("p1") is not None
+    assert result.get_arg_present("p1").slice_string == "hello yo there"
+    assert result.remaining_start_i == len("hello yo there")
+
+
+def test_prog_object_parser_posarg_multword_optional_not_present(type_context):
+    argval = AInixObject(
+        type_context, "FooProgram", "Program",
+        [_make_positional(required=False, multiword=True)])
+    parser = type_context.get_object_parser_by_name("ProgramObjectParser")
+    result = gen_result(parser.parse_string("", argval))
+    assert result.get_arg_present("p1") is None
+    assert result.remaining_start_i == len("")
+
+
 def test_prog_object_parser_posarg_multword_and_nonpos(type_context):
     argval = AInixObject(
         type_context, "FooProgram", "Program",
@@ -341,6 +364,22 @@ def test_string_parse_e2e(type_context):
     assert to_string.total_string == "hello -a"
 
 
+def test_string_parse_e2e2(type_context):
+    twoargs = AInixObject(
+        type_context, "FooProgram", "Program",
+        [AInixArgument(type_context, "a", None, arg_data={"short_name": "a"},
+                       parent_object_name="sdf"),
+         AInixArgument(type_context, "barg", None, arg_data={"short_name": "b"},
+                       parent_object_name="bw")],
+        type_data={"invoke_name": "hello"}
+    )
+    parser = StringParser(type_context)
+    ast = parser.create_parse_tree("hello", "CommandSequence")
+    unparser = AstUnparser(type_context)
+    to_string = unparser.to_string(ast)
+    assert to_string.total_string == "hello"
+
+
 def test_string_parse_e2e_multiword(type_context):
     fooType = AInixType(type_context, "FooType")
     fo = AInixObject(type_context, "fo", "FooType", [],
@@ -364,6 +403,30 @@ def test_string_parse_e2e_multiword(type_context):
     unparser = AstUnparser(type_context)
     to_string = unparser.to_string(ast)
     assert to_string.total_string == "hello -a foo"
+
+
+def test_string_parse_e2e_multiword3(type_context):
+    fooType = AInixType(type_context, "FooType")
+    fo = AInixObject(type_context, "fo", "FooType", [],
+                     preferred_object_parser_name=create_object_parser_from_grammar(
+                         type_context,
+                         "fooname", '"foo"'
+                     ).name)
+    twoargs = AInixObject(
+        type_context, "FooProgram", "Program",
+        [AInixArgument(type_context, "a", None, arg_data={"short_name": "a"},
+                       parent_object_name="sdf"),
+         AInixArgument(type_context, "barg", None, arg_data={"short_name": "b"},
+                       parent_object_name="bw"),
+         _make_positional()],
+        type_data={"invoke_name": "hello"}
+    )
+    type_context.finalize_data()
+    parser = StringParser(type_context)
+    ast = parser.create_parse_tree("hello -a", "CommandSequence")
+    unparser = AstUnparser(type_context)
+    to_string = unparser.to_string(ast)
+    assert to_string.total_string == "hello -a"
 
 
 def test_string_parse_e2e_multiword2(type_context):
@@ -424,5 +487,10 @@ def test_string_parse_e2e_sequence(type_context):
 
     no_space = "hello -a|hello -b"
     ast = parser.create_parse_tree(no_space, "CommandSequence")
+    to_string = unparser.to_string(ast)
+    assert to_string.total_string == string
+
+    string = "hello -a | hello"
+    ast = parser.create_parse_tree(string, "CommandSequence")
     to_string = unparser.to_string(ast)
     assert to_string.total_string == string

commit e226d3e3dc5ae54570bfbd7692c30f5289342a56

fix regression introduced in 40569e672b1f related to extra spaces in unparse and stuff

ainix_common/parsing/parse_primitives.py
@@ -250,13 +250,18 @@ class TypeParserResult:
         self.set_next_slice(si, endi)
 
 
+class RStripRequest:
+    """Used in typetostring result to request the unparser perform a rstrip"""
+    pass
+
+
 class TypeToStringResult:
     def __init__(
         self,
         implementation: 'typecontext.AInixObject',
         type_of_impl: 'typecontext.AInixType'
     ):
-        self._unparse_seq: List[Union[str, ImplementationToStringDelegation]] = []
+        self._unparse_seq: List[Union[str, ImplementationToStringDelegation, RStripRequest]] = []
         self._implementation = implementation
         self._type_of_impl = type_of_impl
         self._already_added_impl = False
@@ -276,9 +281,14 @@ class TypeToStringResult:
     def add_string(self, string: str):
         self._unparse_seq.append(string)
 
+    def add_rstrip(self):
+        self._unparse_seq.append(RStripRequest())
+
     def add_impl_unparse(self, prefix_adder: Callable[[str], str] = None):
         if self._already_added_impl:
             raise ValueError("Cannot add implementation multiple times in unparse")
+        if prefix_adder is not None:
+            raise NotImplemented("This didn't actually work and is not currently supported")
         next_parser = _next_parser_of_impl(self._implementation, self._type_of_impl)
         to_string_del = ImplementationToStringDelegation(next_parser, prefix_adder)
         self._unparse_seq.append(to_string_del)

ainix_common/parsing/stringparser.py
@@ -15,7 +15,7 @@ from ainix_common.parsing.parse_primitives import (TypeParser, ArgParseDelegatio
                                                    TypeToStringResult,
                                                    ImplementationToStringDelegation, ObjectParser,
                                                    ObjectNodeArgMap, ArgToStringDelegation,
-                                                   ArgIsPresentToString)
+                                                   ArgIsPresentToString, RStripRequest)
 from ainix_common.parsing.typecontext import OPTIONAL_ARGUMENT_NEXT_ARG_NAME
 from ainix_common.parsing.model_specific import tokenizers
 import functools
@@ -355,11 +355,15 @@ class AstUnparser:
                 impl_string = self._unparse_object_node(
                     node.next_node_not_copy, part_of_out.next_parser, result_builder,
                     new_left_offset, child_nums_here + (0, ))
-                if part_of_out.prefix_adder:
-                    prefix = part_of_out.prefix_adder(impl_string)
-                    impl_string = prefix + impl_string
+                #if part_of_out.prefix_adder:
+                #    prefix = part_of_out.prefix_adder(impl_string)
+                #    impl_string = prefix + impl_string
                 out_string += impl_string
                 new_left_offset += len(impl_string)
+            elif isinstance(part_of_out, RStripRequest):
+                old_str = out_string
+                out_string = out_string.rstrip()
+                new_left_offset -= len(old_str) - len(out_string)
             else:
                 raise ValueError("Unexpected object in unparse_seq")
         result_builder.add_subspan(node, child_nums_here, out_string, left_offset)

ainix_kernel/models/Fullretrieval/fullretmodel.py
@@ -34,7 +34,7 @@ import sklearn.naive_bayes
 
 import attr
 
-REPLACEMENT_SAMPLES = 5
+REPLACEMENT_SAMPLES = 10
 START_COPY_KERNEL_WEIGHTS = torch.tensor([0.25, 1, 0.05])
 END_COPY_KERNEL_WEIGHTS = torch.tensor([0.05, 1, 0.25])
 #                         ^ Weight current token the most and the before and after less.
@@ -253,7 +253,6 @@ def _preproc_example(
     """
     # Sample a bunch of replacements and get their summary
     x, y = xval.x_text, yval.y_text
-    print(x)
     needs_replacement = replacers.check_if_string_has_replacement_spots(x) or \
                         replacers.check_if_string_has_replacement_spots(y)
     xs, ys = [], []

ainix_kernel/training/fullret_try.py
@@ -21,11 +21,11 @@ if __name__ == "__main__":
     model, index, replacers, type_context, loader = train_the_thing()
     unparser = AstUnparser(type_context, model.get_string_tokenizer())
 
-    #tran_trainer = TypeTranslateCFTrainer(model, index, replacer=replacers, loader=loader)
-    #logger = None
-    #logger = EvaluateLogger()
-    #tran_trainer.evaluate(logger, dump_each=True, num_replace_samples=5)
-    #print_ast_eval_log(logger)
+    tran_trainer = TypeTranslateCFTrainer(model, index, replacer=replacers, loader=loader)
+    logger = None
+    logger = EvaluateLogger()
+    tran_trainer.evaluate(logger, dump_each=True, num_replace_samples=5)
+    print_ast_eval_log(logger)
 
     while True:
         q = input("Query: ")

ainix_kernel/training/train_contexts.py
@@ -20,8 +20,6 @@ def load_all_examples(tc: TypeContext) -> ExamplesStore:
     for f in ALL_EXAMPLE_NAMES:
         if f in ("numbers",):
             continue
-        if f not in ("cat",):
-            continue
         exampleloader.load_path(f"{dirname}/../../builtin_types/{f}_examples.ainix.yaml", index)
     return index
 

builtin_types/posix_parsers.py
@@ -109,10 +109,11 @@ def ProgramTypeParser(
 def ProgramTypeUnparser(result: parse_primitives.TypeToStringResult):
     impl = result.implementation
     prog_name = impl.type_data['invoke_name']
-    result.add_string(prog_name)
+    result.add_string(prog_name + " ")
     result.add_impl_unparse(
-        prefix_adder=lambda s: " " if s else ""
+        #prefix_adder=lambda s: " " if s else ""
     )
+    result.add_rstrip()
 ####
 
 

builtin_types/test_posix_parsers.py
@@ -2,6 +2,7 @@ import random
 
 import pytest
 
+from ainix_common.parsing.ast_components import AstIterPointer, ObjectNode, ObjectChoiceNode
 from ainix_common.parsing.grammar_lang import create_object_parser_from_grammar
 from builtin_types.posix_parsers import *
 from unittest.mock import MagicMock
@@ -451,6 +452,35 @@ def test_string_parse_e2e_multiword2(type_context):
     unparser = AstUnparser(type_context)
     to_string = unparser.to_string(ast)
     assert to_string.total_string == "hello -a foo bar"
+
+
+def test_string_parse_e2e_pos_unparse(type_context):
+    fooType = AInixType(type_context, "FooType")
+    fo = AInixObject(type_context, "fo", "FooType", [],
+                     preferred_object_parser_name=create_object_parser_from_grammar(
+                         type_context,
+                         "fooname", '"foo"'
+                     ).name)
+    twoargs = AInixObject(
+        type_context, "FooProgram", "Program",
+        [AInixArgument(type_context, "p1", "FooType",
+                       arg_data={POSITION: 0, MULTIWORD_POS_ARG: False},
+                       parent_object_name="bw",
+                       required=True)],
+        type_data={"invoke_name": "hello"}
+    )
+    type_context.finalize_data()
+    parser = StringParser(type_context)
+    ast = parser.create_parse_tree("hello foo", "Program")
+    unparser = AstUnparser(type_context)
+    unparse_result = unparser.to_string(ast)
+    assert unparse_result.total_string == "hello foo"
+    for p in ast.depth_first_iter():
+        n = p.cur_node
+        if isinstance(n, ObjectChoiceNode) and n.type_to_choose.name == "FooType":
+            arg_node_pointer = p
+            break
+    assert unparse_result.pointer_to_string(arg_node_pointer) == "foo"
 
 
 def test_command_operator_parser(type_context):